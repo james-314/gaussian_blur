db80cea14f146060e50baf04ddbe685a473883bc
James B <james.r.c.black@gmail.com>
2024-03-27 21:14:17 +0000

    Totally numpy.

--- blur.py
+++ blur.py
@@ -16,39 +16,17 @@ def save_array_as_image(image_as_array: np.array, filename: str) -> None:
 def create_gaussian_kernel(kernel_size: int, sigma: float) -> np.array:
     x = np.linspace(-3.0 * sigma, 3.0 * sigma, kernel_size)
     kernel_1d = (1.0 / (np.sqrt(2.0 * np.pi) * sigma)) * np.exp(-0.5 * np.power(x, 2) / (sigma ** 2))
-    kernel_2d = np.outer(kernel_1d, kernel_1d)
-    kernel_sum = np.sum(kernel_2d)  # this is the total area of the kernel, it should be 1.0 so no power is lost
-    return kernel_2d / kernel_sum  # we normalise for the power of the kernel
+    return kernel_1d / np.sum(kernel_1d)
 
 
-def convolve(a: np.array, b: np.array) -> float:
-    # the matrices must be the same size
-    if a.shape != b.shape:
-        return 0.0
-
-    # they must also be 2-dimensional
-    if len(a.shape) != 2 and len(b.shape) != 2:
-        return 0.0
-
-    multiplied = np.multiply(a, b)
-
-    return np.sum(multiplied)
-
-
-def convolve_image(matrix: np.array, kernel: np.array) -> np.array:
-    kernel_length = kernel.shape[0]
-    kernel_offset = (kernel_length - 1) // 2
+def convolve_image(input_image: np.array, kernel: np.array) -> np.array:
     # create copy
-    convolved_image = np.zeros_like(matrix)
+    convolved_image = np.zeros_like(input_image)
     for colour_index in range(convolved_image.shape[2]):
-        for row in range(kernel_offset, matrix.shape[0] - kernel_offset):
-            for col in range(kernel_offset, matrix.shape[1] - kernel_offset):
-                convolved_image[row, col, colour_index] = convolve(
-                    matrix[row - kernel_offset:row + kernel_offset + 1, col - kernel_offset:col + kernel_offset + 1,
-                    colour_index],
-                    kernel
-                )
-
+        for row in range(input_image.shape[0]):
+            convolved_image[row, :, colour_index] = np.convolve(input_image[row, :, colour_index], kernel, mode='same')
+        for col in range(input_image.shape[1]):
+            convolved_image[:, col, colour_index] = np.convolve(convolved_image[:, col, colour_index], kernel, mode='same')
     return convolved_image
 
 
@@ -57,7 +35,7 @@ def run_demo():
     output_image_name = "blurry dog"
     output_image_extension = ".jpg"
 
-    kernel_size = 5
+    kernel_size = 50
     sigma = 3.0
     kernel = create_gaussian_kernel(kernel_size, sigma)
 
@@ -68,7 +46,7 @@ def run_demo():
     end = time.time_ns()
 
     print(f"convolution took: {(end - start) / 1000_000_000:.3f}s")
-    save_array_as_image(convolved_image, f"{output_image_name}-{kernel_size}k{sigma:.1f}s{output_image_extension}")
+    save_array_as_image(convolved_image, f"alt-{output_image_name}-{kernel_size}k{sigma:.1f}s{output_image_extension}")
 
 
 if __name__ == "__main__":
